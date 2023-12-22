2c4b6f834e32d20e7bea4fbc82050adbb08b908e
Eh-haque <ehtisamul@gmail.com>
2023-12-22 18:33:36 +0600

    pagination added

--- controllers/aiTool.js
+++ controllers/aiTool.js
@@ -1,10 +1,31 @@
 import AITool from "../models/AITool.js";
+import QueryBuilder from "mongoose-dynamic-querybuilder";
 
 const getTools = async (req, res, next) => {
   try {
-    const data = await AITool.find();
+    const userQuery = new QueryBuilder(AITool.find({}), req.query);
+
+    const [data, totalData] = await Promise.all([
+      userQuery
+        .filter()
+        .search(["title", "toolFeature"]) // to make dynamic search use like ['']
+        .sort()
+        .paginate()
+        .fields().modelQuery,
+      userQuery.countTotal(),
+    ]);
+
+    const limit = Number(req.query.limit) || 10;
+    const meta = {
+      limit,
+      page: Number(req.query.page) || 1,
+      total: totalData,
+      totalPage: Math.ceil(totalData / limit),
+    };
+
     res.status(200).send({
       success: true,
+      meta,
       data,
     });
   } catch (error) {
